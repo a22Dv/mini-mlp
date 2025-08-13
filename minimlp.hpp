0442ed33beffe3127a3327bbb43b302b61b02fc5
a22Dv <a22dev.gl@gmail.com>
2025-08-13 10:39:24 +0800

    Added at() calls for the Tensor's API.

--- minimlp.hpp
+++ minimlp.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
 // #define MINIMLP_IMPLEMENTATION // DEVELOPMENT
+
 #ifdef MINIMLP_IMPLEMENTATION
 #endif
 
 #include <cstdint>
 #include <filesystem>
 #include <optional>
-#include <vector>
 #include <stdexcept>
+#include <type_traits>
+#include <utility>
+#include <vector>
 
 namespace mlp {
 
@@ -17,64 +20,86 @@ using u32_t = std::uint32_t;
 using u8_t = std::uint8_t;
 
 /**
-    TODO: Finish Matrix API. Currently at the at() methods.
+    TODO: Finish Tensor API. Currently at the at() methods.
 */
-template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>> class Matrix {
+
+template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>> class Tensor {
   private:
     std::vector<u32_t> _dimensions{};
+    std::vector<u32_t> _strideUnits{};
     std::vector<T> _data{};
 
-  public:
-    Matrix();
-    Matrix(std::vector<T> initializer, std::vector<u32_t> dims) : _data{initializer}, _dimensions{dims} {};
-    std::vector<T> &data() { return _data; };
-    const T& atC(std::size_t ...) const {
-
-    }
-    T& at(std::size_t ...) {
-
-    }
-    const std::vector<T> &cData() const { return _data; };
-    const std::vector<u32_t> &dimensions() const { return _dimensions; };
-    T& operator[](const std::size_t idx) {
+    template <typename... Indices, std::size_t... Dims> T &at(std::index_sequence<Dims...>, Indices... indices) {
+        std::size_t idx{((indices * _strideUnits[Dims]) + ...)};
         return _data[idx];
     }
-    Matrix<T> operator+(const Matrix<T> &rhs) {
+
+  public:
+    Tensor();
+    Tensor(std::vector<u32_t> dims) : _dimensions{dims} {};
+    Tensor(std::vector<T> initializer, std::vector<u32_t> dims) : _data{initializer}, _dimensions{dims} {
+        std::size_t dimsN{_dimensions.size()};
+        _strideUnits.resize(dimsN);
+        if (dimsN == 0) {
+            throw std::runtime_error("Invalid argument. Tensor with a dimension count of 0 is not allowed.");
+        }
+        _strideUnits[dimsN - 1] = 1;
+        for (std::size_t i{dimsN - 1}; i-- > 0;) {
+            _strideUnits[i] = _strideUnits[i + 1] * _dimensions[i + 1];
+        }
+    };
+    std::vector<T> &data() { return _data; };
+    const std::vector<T> &data() const { return _data; };
+    const std::vector<u32_t> &dimensions() const { return _dimensions; };
+
+    template <typename... Indices>
+    auto at(Indices... indices) const
+        -> std::enable_if_t<((std::is_unsigned_v<Indices> && std::is_integral_v<Indices>) && ...), const T &> {
+        return at(std::make_index_sequence<sizeof...(Indices)>(), indices...);
+    }
+    template <typename... Indices>
+    auto at(Indices... indices)
+        -> std::enable_if_t<((std::is_unsigned_v<Indices> && std::is_integral_v<Indices>) && ...), T &> {
+        return at(std::make_index_sequence<sizeof...(Indices)>(), indices...);
+    }
+
+    const T &operator[](const std::size_t idx) const { return _data[idx]; }
+    T &operator[](const std::size_t idx) { return _data[idx]; }
+
+    Tensor<T> operator+(const Tensor<T> &rhs) {
         if (_dimensions != rhs.dimensions()) {
             throw std::runtime_error("Invalid arguments. Arguments' dimensions are not consistent.");
         }
+        Tensor<T> out{_dimensions};
         std::size_t iters{_data.size()};
-        Matrix<T> out{};
         for (std::size_t i{0}; i < iters; ++i) {
-            
         }
     }
-    Matrix<T> operator+(const T scalar) {
+    Tensor<T> operator+(const T scalar) {
+        Tensor<T> out{};
         for (T val : _data) {
             val += scalar;
         }
     }
-    Matrix<T> operator-(const Matrix<T> &rhs);
-    Matrix<T> operator-(const T scalar) {
+    Tensor<T> operator-(const Tensor<T> &rhs);
+    Tensor<T> operator-(const T scalar) {
         for (T val : _data) {
             val -= scalar;
         }
     }
-    Matrix<T> operator*(const Matrix<T> &rhs);
-    Matrix<T> operator*(const T scalar) {
+    Tensor<T> operator*(const Tensor<T> &rhs);
+    Tensor<T> operator*(const T scalar) {
         for (T val : _data) {
             val *= scalar;
         }
     }
-    Matrix<T> operator/(const Matrix<T> &rhs);
-    Matrix<T> operator/(const T scalar) {
+    Tensor<T> operator/(const Tensor<T> &rhs);
+    Tensor<T> operator/(const T scalar) {
         for (T val : _data) {
             val /= scalar;
         }
     }
-    void hadamard(const Matrix<T> &rhs) {
-
-    }
+    void hadamard(const Tensor<T> &rhs) {}
     void transpose();
 };
 
@@ -102,20 +127,20 @@ enum class ModelOptimizationFunction_ : u8_t {
 
 template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>> struct ModelActivationFunction {
     ModelActivationFunction_ type{};
-    Matrix<T> out(const Matrix<T> &logits);
+    Tensor<T> out(const Tensor<T> &logits);
 };
 
 template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>> struct ModelLossFunction {
     ModelLossFunction_ type{};
-    f32_t out(const Matrix<T> &modelOut, const Matrix<T> &expected);
+    f32_t out(const Tensor<T> &modelOut, const Tensor<T> &expected);
 };
 
 template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>> struct Layer {
     ModelActivationFunction<T> actFunc{};
-    Matrix<T> weights{};
-    Matrix<T> biases{};
-    std::optional<Matrix<T>> intermediate{};
-    Matrix<T> out(const Matrix<T> &input);
+    Tensor<T> weights{};
+    Tensor<T> biases{};
+    std::optional<Tensor<T>> intermediate{};
+    Tensor<T> out(const Tensor<T> &input);
 };
 
 template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>> struct ModelOptimizationFunction {
